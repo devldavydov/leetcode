5b77d3a472fddab027afdab7c00bd39361b45983
Denis Davydov <denis.davydov@kaspersky.com>
2023-11-01 19:30:05 +0300

    Add easy/merge two sorted lists test

---
+++ go/leetcode/easy/merge_two_sorted_lists.go
@@ -0,0 +1,18 @@
+package easy
+
+/*
+https://leetcode.com/problems/merge-two-sorted-lists/
+
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+Return the head of the merged linked list.
+*/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func MergeTwoSortedLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	return nil
+}
---
+++ go/leetcode/easy/merge_two_sorted_lists_test.go
@@ -0,0 +1,34 @@
+package easy
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMergeTwoSortedLists(t *testing.T) {
+	assert.Equal(t, []int{1, 1, 2, 3, 4, 4}, sliceFromList(MergeTwoSortedLists(listFromSlice([]int{1, 2, 4}), listFromSlice([]int{1, 3, 4}))))
+	assert.Equal(t, []int{}, sliceFromList(MergeTwoSortedLists(listFromSlice([]int{}), listFromSlice([]int{}))))
+	assert.Equal(t, []int{0}, sliceFromList(MergeTwoSortedLists(listFromSlice([]int{}), listFromSlice([]int{0}))))
+}
+
+func listFromSlice(sl []int) *ListNode {
+	var prev *ListNode
+	for _, i := range sl {
+		node := &ListNode{Val: i, Next: nil}
+		if prev != nil {
+			prev.Next = node
+		}
+		prev = node
+	}
+	return prev
+}
+
+func sliceFromList(p *ListNode) []int {
+	sl := make([]int, 0)
+	for p != nil {
+		sl = append(sl, p.Val)
+		p = p.Next
+	}
+	return sl
+}
